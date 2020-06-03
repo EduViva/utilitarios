cfd96903410aa334790b0beb490d91c5995add19
EduViva <miissjackson72@gmail.com>
2020-06-02 22:00:30 -0300

    lista updated

--- funcs-lista.js
+++ funcs-lista.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function() {
     let butLista = document.getElementById("lista-add");
     let input = document.getElementById("lista-inp");
     var saida = document.getElementById("saida"); 
+    var counts = document.getElementById("counts");
     var count = 0;
 
     input.addEventListener("keypress" , (e) => {
@@ -12,11 +13,13 @@ window.addEventListener('load', function() {
     });
 
     butLista.addEventListener("click", add);
-    
+
     function add(){
         let val = document.getElementById("lista-inp").value;
         document.getElementById("lista-inp").value = "";
         
+        let counts = document.getElementById("counts");
+    
         if(val){
             
             //Criando a checkbox
@@ -33,6 +36,15 @@ window.addEventListener('load', function() {
             label.id = `label-item-${count}`;
             label.htmlFor = `item-${count}`;
             label.appendChild(document.createTextNode(val));
+            
+            //Criando o counter
+            let counterIt = document.createElement("p");
+            let span = document.createElement("span");
+            counterIt.id = `count-${count}`;
+            span.id = `span-${count}`;
+            counterIt.appendChild(document.createTextNode(val + ": "));
+            counterIt.appendChild(span);
+            counts.appendChild(counterIt);
 
             //Criando a div
             let div = document.createElement("div");
@@ -53,8 +65,18 @@ window.addEventListener('load', function() {
     }
 
     function changePos(e){
+        
         let id = e.target.id;
+        let idN = id.split("-")[1];
 
+        let actual = document.getElementById(`span-${idN}`);
+        
+        if (!actual.textContent){
+            actual.textContent = 1;
+        } else {
+            actual.textContent = Number(actual.textContent) + 1;
+        }
+        
         let box = document.getElementById(id);
         let label = document.getElementById(`label-${id}`);
         let div = document.getElementById(`div-${id}`);
